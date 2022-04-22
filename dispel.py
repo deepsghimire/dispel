7670bcf5d6d89ac29d928ef3547eeffc415d6a3b
stengah <deepsghimire1@gmail.com>
2022-04-22 07:47:42 +0545

    Old way of correcting(previous versions) had a flaw. Language model must be taken into account for proper text correction. This technique is inspired by http://norvig.com/spell-correct.html

--- dispel.py
+++ dispel.py
@@ -5,6 +5,8 @@ import os
 import bisect
 import sys
 import logging
+from collections import Counter
+import re
 
 def parse_args():
     parser = argparse.ArgumentParser(description="prints possible correction of word")
@@ -26,32 +28,55 @@ class Corrector:
         self.all_words = self._read_words()
 
     def _read_words(self):
+
        
         logging.info(f"the words  are in {self.words_file}")
 
         if os.path.isfile(self.words_file):
-            words = open(self.words_file).read().split('\n')
+            words = Counter(re.findall(r'\w+',open(self.words_file).read()))
             logging.info(f"Words file contains {len(words)} words")
         else:
             logging.critical(f"{self.words_file} is not a file")
             raise WordsFileError(f"Cannot read word file {self.words_file}")
         return words
 
-    def _word_exists(self,word):
-        i = bisect.bisect_left(self.all_words,word)
-        if i != len(self.all_words) and self.all_words[i] == word:
-            return True
-        return False
+
+    def known(self,words):
+        return set(w for w in words if w in self.all_words)
+
+    def edits1(self,word):
+        "All edits that are one edit away from 'word'."
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        splits = [(word[:i],word[i:]) for i in range(len(word)+1) ]
+        deletes = [L + R[1:] for L,R in splits if R]
+        transposes = [L + R[1] + R[0] + R[2:] for L,R in splits if len(R) > 1]
+        replaces = [L + c + R[1:] for L,R in splits if R for c in letters]
+        inserts = [L + c + R for L,R in splits for c in letters]
+        return set(deletes + transposes + replaces + inserts)
+
+    def edits2(self,word):
+        "All edits that are two edits away from 'word'."
+        return (e2 for e1 in self.edits1(word) for e2 in self.edits1(e1))
+        
+
+
+    def _prob(self,word):
+        return self.all_words[word]/sum(self.all_words.values())
+
+    def candidates(self,word):
+        return self.known([word]) or self.known(self.edits1(word)) or self.known(self.edits2(word))
             
 
     def correct(self,input_word):
         
         logging.info(f"The input text is: {input_word}")
 
-        if self._word_exists(input_word):
-            return [input_word]
+        candidates = self.candidates(input_word)
+        if candidates:
+            return max(candidates,key=self._prob)
         else:
-            return []
+            return ''
+
 
 
 
@@ -62,21 +87,20 @@ def main():
     #word is the only argument for now
     configure_logs()
     args = parse_args()
-    wordfile = "words.txt"
+    wordfile = "big.txt"
     
     input_word = args.word
     
 
     corrector = Corrector(wordfile)
 
-    possible_corrections = corrector.correct(input_word)
-    for each in possible_corrections:
-        print(each)
+    possible_correction = corrector.correct(input_word)
+    print(possible_correction)
 
     # correctly exit
 
-    logging.info(f" 10 possible_corrections: {possible_corrections[:10]}")
-    if len(possible_corrections) > 0:
+    logging.info(f" possible_correction: {possible_correction}")
+    if len(possible_correction) > 0:
         sys.exit(0)
     else:
         sys.exit(1)
